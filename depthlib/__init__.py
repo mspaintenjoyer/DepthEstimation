0784d64c95a3dc01b6adb94eecafcc405b173459
Piyush Gupta <piyushgupta941d@gmail.com>
2025-12-05 18:40:51 +0530

    Adds monocular depth estimator

--- depthlib/__init__.py
+++ depthlib/__init__.py
@@ -1,4 +1,5 @@
 from depthlib.StereoDepthEstimator import StereoDepthEstimator
+from depthlib.MonocularDepthEstimator import MonocularDepthEstimator
 from depthlib.visualizations import (visualize_stereo_pair, visualize_disparity, 
                             visualize_depth, visualize_disparity_and_depth)
 
